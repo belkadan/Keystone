4bf30243b3cb057a35a74c8e1bb2e4ef9a188485
jediknil <jediknil@ba59ac2a-6797-437e-b805-98a2da23c0da>
2009-12-24 03:35:15 +0000

    Documentation
    git-svn-id: svn+belkadan://belkadan.com/jr/cocoa/keystone/devel@79 ba59ac2a-6797-437e-b805-98a2da23c0da

--- AddCompletionController.h
+++ AddCompletionController.h
@@ -2,13 +2,26 @@
 #import "QueryCompletionItem.h"
 #import "SheetRequest.h"
 
+/*!
+ * Handles the popup for adding new completions, and reports back to the main
+ * controller once the user has made a choice.
+ */
 @interface ComBelkadanKeystone_AddCompletionController : NSWindowController <ComBelkadanKeystone_SheetRequest> {
 	ComBelkadanKeystone_QueryCompletionItem *newCompletion;
 }
+/*! 
+ * Creates a new controller for the given site name and URL, using the first word
+ * of the site name as the keyword. (The actual part of the name used to generate
+ * the keyword may change.)
+ */
 - (id)initWithName:(NSString *)name URL:(NSString *)completionURL;
+
+/*! Creates a new controller for the given site name, suggested keyword, and URL. */
 - (id)initWithName:(NSString *)name keyword:(NSString *)keyword URL:(NSString *)completionURL;
 
+/*! The completion being edited. */
 @property(readonly) ComBelkadanKeystone_QueryCompletionItem *newCompletion;
 
+/*! Called to close the sheet. The tag of the sender is used as the return code. */
 - (IBAction)close:(id)sender;
 @end
