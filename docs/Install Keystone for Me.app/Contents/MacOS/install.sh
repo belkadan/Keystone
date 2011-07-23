96e0eb18be97e9ffc8b5422e3b97171f313c758b
jediknil <jediknil@ba59ac2a-6797-437e-b805-98a2da23c0da>
2011-07-23 00:40:12 +0000

    Make the install script nicer, with a link to SIMBL on success.
    git-svn-id: svn+belkadan://belkadan.com/jr/cocoa/keystone/devel@212 ba59ac2a-6797-437e-b805-98a2da23c0da

--- docs/Install Keystone for Me.app/Contents/MacOS/install.sh
+++ docs/Install Keystone for Me.app/Contents/MacOS/install.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
+SIMBL_URL='http://www.culater.net/software/SIMBL/SIMBL.php'
+
 containing_folder=$(dirname "$(dirname "$(dirname "$(dirname "$0")")")")
 SIMBL_folder=$HOME/'Library/Application Support/SIMBL/Plugins'
 
-mkdir -p "$SIMBL_folder"
-cp -RPf "$containing_folder/Keystone.bundle" "$SIMBL_folder"
+if mkdir -p "$SIMBL_folder"; then
+	if [[ -e "$SIMBL_folder/Keystone.bundle" ]]; then
+		rm -rf "$SIMBL_folder/Keystone.bundle"
+	fi
+
+	if cp -RPf "$containing_folder/Keystone.bundle" "$SIMBL_folder"; then
+		should_get_SIMBL=$(osascript <<SCRIPT
+			tell app "Finder"
+				activate
+				set userChoice to display alert "Keystone has been installed." message "If Keystone is not loaded when you next start Safari, you may need to install a recent version of SIMBL." buttons {"Get SIMBL", "OK"}
+				if button returned of userChoice is "Get SIMBL"
+					true
+				end
+			end
+SCRIPT
+		)
+
+		if [[ ! -z "$should_get_SIMBL" ]]; then
+			open "$SIMBL_URL"
+		fi
+
+		exit 0
+	fi
+fi
+
+osascript <<SCRIPT
+	tell app "Finder"
+		activate
+		display alert "There was a problem installing Keystone." message "You can install it manually by copying Keystone.bundle to ~/Library/Application Support/SIMBL/Plugins, creating that folder if necessary."
+	end
+SCRIPT
